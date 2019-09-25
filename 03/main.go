ab6e2fd7225da45cbe72b5b1df0c3bdd6a207b4c
Roger Chapman <rogchap@gmail.com>
2019-09-25 22:07:24 +1000

    implement the simple forward

--- 03/main.go
+++ 03/main.go
@@ -6,14 +6,34 @@ import (
 	"syscall"
 )
 
-func echo(fd int, sa syscall.Sockaddr) {
+func handler(fd int, sa syscall.Sockaddr) {
 	defer syscall.Close(fd)
 
+	// get mesage from caller
 	b := make([]byte, 1500)
 	if _, _, err := syscall.Recvfrom(fd, b, 0); err != nil {
 		panic(err)
 	}
 
+	// proxy the message
+	nfd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer syscall.Close(nfd)
+
+	nsa := &syscall.SockaddrInet4{
+		Port: 9000,
+		Addr: [...]byte{0, 0, 0, 0},
+	}
+	if err := syscall.Connect(nfd, nsa); err != nil {
+		panic(err)
+	}
+	if err := syscall.Sendto(nfd, b, 0, nsa); err != nil {
+		panic(err)
+	}
+
+	// respond to the caller
 	if err := syscall.Sendto(fd, b, 0, sa); err != nil {
 		panic(err)
 	}
@@ -46,7 +66,7 @@ func main() {
 			if err != nil {
 				panic(err)
 			}
-			go echo(nfd, dsa)
+			go handler(nfd, dsa)
 		}
 	}()
 
